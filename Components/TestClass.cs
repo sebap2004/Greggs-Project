5c9e15c8630a7f147264bd66cc144cbfb50a2eea
sebap <prej2_22@uni.worc.ac.uk>
2025-02-17 19:32:25 +0000

    Updated docfx settings to work properly

--- Components/TestClass.cs
+++ Components/TestClass.cs
@@ -1,5 +1,14 @@
 ﻿namespace SoftwareProject.Components;
-
+/// <summary>
+/// Calculates the age of a person on a certain date based on the supplied date of birth.  Takes account of leap years,
+/// using the convention that someone born on 29th February in a leap year is not legally one year older until 1st March
+/// of a non-leap year.
+/// </summary>
+/// <param name="dateOfBirth">Individual's date of birth.</param>
+/// <param name="date">Date at which to evaluate age at.</param>
+/// <returns>Age of the individual in years (as an integer).</returns>
+/// <remarks>This code is not guaranteed to be correct for non-UK locales, as some countries have skipped certain dates
+/// within living memory.</remarks>
 public class TestClass
 {
     /// <summary>
@@ -12,8 +21,21 @@ public class TestClass
     /// <returns>Age of the individual in years (as an integer).</returns>
     /// <remarks>This code is not guaranteed to be correct for non-UK locales, as some countries have skipped certain dates
     /// within living memory.</remarks>
-    void Hell(DateTime dateOfBirth, DateTime date)
-    {
+    private int Cracker { get; set; }
     
+    
+    /// <summary>
+    /// Calculates the age of a person on a certain date based on the supplied date of birth.  Takes account of leap years,
+    /// using the convention that someone born on 29th February in a leap year is not legally one year older until 1st March
+    /// of a non-leap year.
+    /// </summary>
+    /// <param name="dateOfBirth">Individual's date of birth.</param>
+    /// <param name="date">Date at which to evaluate age at.</param>
+    /// <returns>Age of the individual in years (as an integer).</returns>
+    /// <remarks>This code is not guaranteed to be correct for non-UK locales, as some countries have skipped certain dates
+    /// within living memory.</remarks>
+    public void Hell(DateTime dateOfBirth, DateTime date)
+    {
+        
     }
 }
