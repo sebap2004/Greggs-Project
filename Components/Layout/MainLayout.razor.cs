6fc72329f0ad219e89d50e81371dadbb1d542652
prej2_22 <prej2_22@uni.worc.ac.uk>
2025-03-03 16:46:21 +0000

    added fonts, created a nice front page

--- Components/Layout/MainLayout.razor.cs
+++ Components/Layout/MainLayout.razor.cs
@@ -1,5 +1,6 @@
 ﻿using MudBlazor;
 using Microsoft.AspNetCore.Components;
+using SoftwareProject.Themes;
 
 namespace SoftwareProject.Components.Layout;
 public partial class MainLayout : LayoutComponentBase
@@ -12,12 +13,7 @@ public partial class MainLayout : LayoutComponentBase
     {
         base.OnInitialized();
 
-        _theme = new()
-        {
-            PaletteLight = _lightPalette,
-            PaletteDark = _darkPalette,
-            LayoutProperties = new LayoutProperties()
-        };
+        _theme = new DefaultTheme();
     }
 
     private void DrawerToggle()
@@ -30,59 +26,9 @@ public partial class MainLayout : LayoutComponentBase
         _isDarkMode = !_isDarkMode;
     }
 
-    private readonly PaletteLight _lightPalette = new()
-    {
-        Black = "#110e2d",
-        AppbarText = "#424242",
-        AppbarBackground = "rgba(255,255,255,0.8)",
-        DrawerBackground = "#ffffff",
-        GrayLight = "#e8e8e8",
-        GrayLighter = "#f9f9f9",
-    };
-
-    private readonly PaletteDark _darkPalette = new()
-    {
-        Primary = "#7e6fff",
-        Surface = "#1e1e2d",
-        Background = "#1a1a27",
-        BackgroundGray = "#151521",
-        AppbarText = "#92929f",
-        AppbarBackground = "rgba(26,26,39,0.8)",
-        DrawerBackground = "#1a1a27",
-        ActionDefault = "#74718e",
-        ActionDisabled = "#9999994d",
-        ActionDisabledBackground = "#605f6d4d",
-        TextPrimary = "#b2b0bf",
-        TextSecondary = "#92929f",
-        TextDisabled = "#ffffff33",
-        DrawerIcon = "#92929f",
-        DrawerText = "#92929f",
-        GrayLight = "#2a2833",
-        GrayLighter = "#1e1e2d",
-        Info = "#4a86ff",
-        Success = "#3dcb6c",
-        Warning = "#ffb545",
-        Error = "#ff3f5f",
-        LinesDefault = "#33323e",
-        TableLines = "#33323e",
-        Divider = "#292838",
-        OverlayLight = "#1e1e2d80",
-    };
-
     public string DarkLightModeButtonIcon => _isDarkMode switch
     {
-        true => Icons.Material.Rounded.AutoMode,
+        true => Icons.Material.Rounded.LightMode,
         false => Icons.Material.Outlined.DarkMode,
-    };
-    
-    MudTheme DefaultTheme = new MudTheme()
-    {
-        Typography = new Typography()
-        {
-            Default = new DefaultTypography()
-            {
-                FontFamily = ["Poppins", "Helvetica", "Arial", "sans-serif"]
-            }
-        }
     };
 }
