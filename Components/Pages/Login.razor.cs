7c9eda15d528f9c7933d473544a673be989735b4
WILM9 <mattwilkins.dev@gmail.com>
2025-04-08 12:57:19 +0100

    Added error handling to login and registration. Registration is now also asynchronous.

--- Components/Pages/Login.razor.cs
+++ Components/Pages/Login.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using SoftwareProject.Data;
 using SoftwareProject.Services;
@@ -17,11 +18,18 @@ public partial class Login : ComponentBase
     /// Takes the information a user input in the form and calls the CRUD operation for the account table.
     /// </summary>
     /// <param name="editContext">Tracks the changes made in the form</param>
-    public void registerAccount(EditContext editContext)
+    public async Task registerAccount(EditContext editContext)
     {
-        var newAccount = (Account)editContext.Model;
-        newAccount.account_id = 0;
-        accountService.CreateAccount(newAccount);
+        try
+        {
+            var newAccount = (Account)editContext.Model;
+            newAccount.account_id = 0;
+            await accountService.CreateAccount(newAccount);
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine($"Error Connecting to Database: \n{e.Message}");
+        }
     }
     
     /// <summary>
@@ -29,15 +37,20 @@ public partial class Login : ComponentBase
     /// </summary>
     private async Task LoginSubmit()
     {
-        var checkAccount = await accountService.LoginAccount(account.email, account.password);
-
-        if (checkAccount != null)
+        try
         {
-            navigationManager.NavigateTo("/");
+            var checkAccount = await accountService.LoginAccount(account.email, account.password);
+
+            if (checkAccount != null)
+            {
+                navigationManager.NavigateTo("/");
+            }
+        }
+        catch (SqlException e)
+        {
+            Console.WriteLine($"Error Connecting to Database: \n{e.Message}");
         }
     }
-    
-    
 }
 
 public class LoginModel
