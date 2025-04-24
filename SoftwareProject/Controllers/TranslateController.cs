6e8f26e4531a32f52577d590b89d40fa2ec5804d
prej2_22 <prej2_22@uni.worc.ac.uk>
2025-04-24 01:50:39 +0100

    Added XML comments

--- SoftwareProject/Controllers/TranslateController.cs
+++ SoftwareProject/Controllers/TranslateController.cs
@@ -3,10 +3,20 @@ using Google.Cloud.Translate.V3;
 using Microsoft.AspNetCore.Mvc;
 using SoftwareProject.Client.Data;
 
+
+/// <summary>
+/// API controller to translate text.
+/// </summary>
 [ApiController]
 [Route("api/[controller]")]
 public class TranslateController : ControllerBase
 {
+    
+    /// <summary>
+    /// Uses Google Translate API to translate text.
+    /// </summary>
+    /// <param name="bodyRequest">Translation request. Stores text to be translated as well as language code.</param>
+    /// <returns></returns>
     [HttpPost]
     public async Task<IActionResult> Translate([FromBody] TranslateRequest bodyRequest)
     {
